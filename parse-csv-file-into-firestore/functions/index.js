cb07521ff4381e1abc23d3b3f4fa22c301ac0d1d
mohantalachutla <mohan.talachutla@outlook.com>
2022-06-22 00:49:21 +0530

    download file from storage on upload

--- parse-csv-file-into-firestore/functions/index.js
+++ parse-csv-file-into-firestore/functions/index.js
@@ -1,9 +1,48 @@
+require("dotenv").config()
+if(!process.env?.DOT_ENV_CHECK) throw new Error(".env file is missing")
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const os = require("os")
+const path = require("path")
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+// default region us-central1
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!");
+  response.send("Hello from Firebase!");
+});
+
+const logger = functions.logger
+
+exports.parseCSV = functions
+    .region("asia-southeast1")
+    .storage.object()
+    .onFinalize(async (object, context) => {
+        try {
+            logger.log("context "+ JSON.stringify(context))
+            logger.log("object "+ JSON.stringify(object))
+
+            const {bucket, name: filePath, contentType, metageneration} = object
+
+            const tempFilePath = path.join(os.tmpdir(), path.basename(filePath))
+            await admin.storage().bucket(bucket).file(filePath).
+                download({
+                    destination: tempFilePath
+                })
+            logger.log(`file successfully downloaded at ${tempFilePath}`)
+        } catch (err) {
+            logger.error(err)
+        }
+
+})
+
+
+admin.initializeApp({
+    databaseURL: process.env.FB_DATABASE_URL,
+    storageBucket: process.env.FB_STORAGE_URL
+})
+
